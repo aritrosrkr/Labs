e6420f3046b49511ccc34fa615aaff1d8823cad0
aritrosrkr <aritrosrkr9980@gmail.com>
2025-06-20 12:45:20 +0600

    CPP

---
+++ CSE110/C++/Lab_1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // int l;
+    // int b;
+    // cin >> l >> b;
+    // cout << l*b << endl;
+
+    // int r = l;
+    // cout << r*pow(3.14, 2);
+
+
+
+
+    // /* Write a Java code where given an integer
+    // we need to print the last 2 digits of that number.*/
+    
+    // string str;
+    // cin >> str;
+    // cout << str.length() << endl;
+    // cout << str.substr(str.length()-2) << endl;
+
+
+
+
+    // /*Write a Java program that given a number in inches 
+    // (you have to declare and initialize it yourself) 
+    // converts it to meters.*/
+
+    // int inch;
+    // cin >> inch;
+
+    // double metre = inch * 0.0254;
+    // cout << metre;
+
+
+
+    // /*Write a Java program declaring two integer variables 
+    // and initializing them. Your task is to swap the 
+    // values of these two variables.*/
+    // int var1 = 4;
+    // int var2 = 5;
+    
+    // var1 = var1 + var2;
+    // var2 = var1 - var2;
+    // var1 = var1 - var2;
+
+    // cout << var1 << var2;
+
+
+
+
+    // /*Write a Java program to convert minutes into years and days. 
+    // For simplicity, assume each year consists of 365 days.*/
+    // int min;
+    // int y;
+    // int d;
+    // cin >> min;
+    // y = min / 525600;
+    // d = (min % 525600) / (24*60);
+    // cout << y << " years, " << d << " days.";
+
+
+
+
+    int a = 2;
+    int b = 5;
+    int c = 8;
+
+    double d = (2*b + (c - a)/3) + 7;
+    cout << d;
+
+    return 0;
+}
---
+++ CSE110/C++/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << "\n";
+    cout << "\"Goodbye\"," << "\tWorld!" << endl;
+    cout << "Gurt: \"Yo i got " << 5 << " coins\"";
+}
+
+// Just a comment
+
+/* This is how you do a
+
+multi-line comment*/
